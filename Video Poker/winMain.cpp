7e5578f55f8a722a96423065604dc9ccaa904535
sxales <sxales@hotmail.com>
2026-07-19 14:37:28 -0600

    Added winMain VisualStudio won't do windows app without (even though it did in the old version)

---
+++ Video Poker/winMain.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include "Game.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	Game game = Game("Data/config.txt");
+
+	game.run();
+
+	return 0;
+}
